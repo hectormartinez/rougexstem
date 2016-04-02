f9ced33ae117fddb5d162d396cce5247f6aeeed0
hectormartinez <hector.martinez.a@gmail.com>
2016-04-02 22:49:09 +0200

    unigrams

--- taln2016/icsisumm-primary-sys34_v1/taln2016.sh
+++ taln2016/icsisumm-primary-sys34_v1/taln2016.sh
@@ -15,10 +15,10 @@ echo "INPUTDIR "$INPUTDIR
 echo "DTYPE "$DTYPE
 echo $LENGTH
 
-#OUTPUTDIR=taln2016/duc04_words/test/
-#if [ ! -d "$OUTPUTDIR" ]; then
-#mkdir -p $OUTPUTDIR
-#fi
+OUTPUTDIR=taln2016/duc04_words/test/
+if [ ! -d "$OUTPUTDIR" ]; then
+mkdir -p $OUTPUTDIR
+fi
 
 #python -u summarizer/inference_taln2016.py -b $LENGTH -i $DOCSDIR -o $OUTPUTDIR -t $DTYPE --manpath $REFDIR --decoder glpsolve --timelimit $TIMELIMIT --unigrams --lang en
 #python -u eval_taln.py r1_$DTYPE $OUTPUTDIR'summary/' $LENGTH $SCORES $REFDIR $DTYPE
@@ -40,11 +40,11 @@ echo $LENGTH
 #python -u summarizer/inference_taln2016.py -b $LENGTH -i $DOCSDIR -o $OUTPUTDIR -t $DTYPE --manpath $REFDIR --decoder glpsolve --timelimit $TIMELIMIT --fourgrams --lang en
 #python -u eval_taln.py r4_$DTYPE $OUTPUTDIR'summary/' $LENGTH $SCORES $REFDIR $DTYPE
 
-OUTPUTDIR=taln2016/duc04_su4/test/
-if [ ! -d "$OUTPUTDIR" ]; then
-mkdir -p $OUTPUTDIR
-fi
+#OUTPUTDIR=taln2016/duc04_su4/test/
+#if [ ! -d "$OUTPUTDIR" ]; then
+#mkdir -p $OUTPUTDIR
+#fi
 
-python -u summarizer/inference_taln2016.py -b $LENGTH -i $DOCSDIR -o $OUTPUTDIR -t $DTYPE --manpath $REFDIR --decoder glpsolve --timelimit $TIMELIMIT --su4 --lang en
-python -u eval_taln.py su4_$DTYPE $OUTPUTDIR'summary/' $LENGTH $SCORES $REFDIR $DTYPE
+#python -u summarizer/inference_taln2016.py -b $LENGTH -i $DOCSDIR -o $OUTPUTDIR -t $DTYPE --manpath $REFDIR --decoder glpsolve --timelimit $TIMELIMIT --su4 --lang en
+#python -u eval_taln.py su4_$DTYPE $OUTPUTDIR'summary/' $LENGTH $SCORES $REFDIR $DTYPE
 
