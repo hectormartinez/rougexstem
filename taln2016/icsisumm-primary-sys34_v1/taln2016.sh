327e47cb25e2392f9cd4dc1865adeeb16dbd1b93
hectormartinez <hector.martinez.a@gmail.com>
2016-04-02 22:53:40 +0200

    unigrams

--- taln2016/icsisumm-primary-sys34_v1/taln2016.sh
+++ taln2016/icsisumm-primary-sys34_v1/taln2016.sh
@@ -20,8 +20,8 @@ if [ ! -d "$OUTPUTDIR" ]; then
 mkdir -p $OUTPUTDIR
 fi
 
-#python -u summarizer/inference_taln2016.py -b $LENGTH -i $DOCSDIR -o $OUTPUTDIR -t $DTYPE --manpath $REFDIR --decoder glpsolve --timelimit $TIMELIMIT --unigrams --lang en
-#python -u eval_taln.py r1_$DTYPE $OUTPUTDIR'summary/' $LENGTH $SCORES $REFDIR $DTYPE
+python -u summarizer/inference_taln2016.py -b $LENGTH -i $DOCSDIR -o $OUTPUTDIR -t $DTYPE --manpath $REFDIR --decoder glpsolve --timelimit $TIMELIMIT --unigrams --lang en
+python -u eval_taln.py r1_$DTYPE $OUTPUTDIR'summary/' $LENGTH $SCORES $REFDIR $DTYPE
 
 
 #OUTPUTDIR=taln2016/duc04_bigrams/test/
