1956cb3d7ed04281726d57395eaf74fcc6f918e5
hectormartinez <hector.martinez.a@gmail.com>
2016-04-04 11:54:44 +0200

    taln

--- taln2016/icsisumm-primary-sys34_v1/taln2016.sh
+++ taln2016/icsisumm-primary-sys34_v1/taln2016.sh
@@ -10,8 +10,8 @@ TIMELIMIT=100
 #DOCSDIR=/coastal/summ_data/DUC2004/duc04/docs/
 #REFDIR=/coastal/summ_data/DUC2004/duc04/sums/
 
-DOCSDIR=/home/alonso/proj/rougexstem/Corpus_RPM2_resumes/Corpus_RPM2_documents/smallset/
-REFDIR=/home/alonso/proj/rougexstem/Corpus_RPM2_resumes/Corpus_RPM2_references/smallset/
+DOCSDIR=/home/alonso/proj/rougexstem/Corpus_RPM2_resumes/Corpus_RPM2_documents/test/
+REFDIR=/home/alonso/proj/rougexstem/Corpus_RPM2_resumes/Corpus_RPM2_references/test/
 DTYPE=duc04
 LENGTH=665
 echo "INPUTDIR "$INPUTDIR
@@ -23,17 +23,17 @@ if [ ! -d "$OUTPUTDIR" ]; then
 mkdir -p $OUTPUTDIR
 fi
 
-python -u summarizer/inference_taln2016.py -b $LENGTH -i $DOCSDIR -o $OUTPUTDIR -t $DTYPE --manpath $REFDIR --decoder glpsolve --timelimit $TIMELIMIT --unigrams --lang en
+python -u summarizer/inference_taln2016.py -l $LENGTH -i $DOCSDIR -o $OUTPUTDIR -t $DTYPE --manpath $REFDIR --decoder glpsolve --timelimit $TIMELIMIT --unigrams --lang en
 python -u eval_taln.py r1_$DTYPE $OUTPUTDIR'summary/' $LENGTH $SCORES $REFDIR $DTYPE
 
 
-#OUTPUTDIR=taln2016/duc04_bigrams/test/
-#if [ ! -d "$OUTPUTDIR" ]; then
-#mkdir -p $OUTPUTDIR
-#fi
+OUTPUTDIR=taln2016/duc04_bigrams/test/
+if [ ! -d "$OUTPUTDIR" ]; then
+mkdir -p $OUTPUTDIR
+fi
 
-#python -u summarizer/inference_taln2016.py -b $LENGTH -i $DOCSDIR -o $OUTPUTDIR -t $DTYPE --manpath $REFDIR --decoder glpsolve --timelimit $TIMELIMIT --bigrams --lang en
-#python -u eval_taln.py r2_$DTYPE $OUTPUTDIR'summary/' $LENGTH $SCORES $REFDIR $DTYPE
+python -u summarizer/inference_taln2016.py -l $LENGTH -i $DOCSDIR -o $OUTPUTDIR -t $DTYPE --manpath $REFDIR --decoder glpsolve --timelimit $TIMELIMIT --bigrams --lang en
+python -u eval_taln.py r2_$DTYPE $OUTPUTDIR'summary/' $LENGTH $SCORES $REFDIR $DTYPE
 
 #OUTPUTDIR=taln2016/duc04_fourgrams/test/
 #if [ ! -d "$OUTPUTDIR" ]; then
